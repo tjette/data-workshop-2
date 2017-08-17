eb08e3461cad69647793aa86a14258f1c110278a
Jay Cheroske <jay@portal.org>
2017-08-17 10:39:04 -0700

    getProductsForOrder

---
+++ src/functions/getProductsForOrder.js
@@ -0,0 +1,17 @@
+export default (data, id) => {
+  if (!data) {
+    throw new Error('getProductsForOrder(): data param is missing')
+  }
+
+  if (!data.orders) {
+    throw new Error('getProductsForOrder(): data.orders is missing')
+  }
+
+  const order = data.orders.find(o => o.id === id)
+
+  console.log(id, order)
+
+  if (!order) return null
+
+  return order.products.map(productId => data.products.find(p => p.id === productId))
+}
